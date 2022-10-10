6e2ed3a962a5b8fc56d63082877bab486b723e23
Soheil <jafarnejad.official@gmail.com>
2022-10-10 20:33:19 +0330

    Add landing page

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -3,7 +3,7 @@ import Category from "@/components/blogs/Category";
 import Sort from "@/components/blogs/Sort";
 import axios from "axios";
 
-const BlogsPage = ({ posts, categories }) => {
+const BlogsPage = ({ posts=[], categories=[] }) => {
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="grid gap-4 grid-cols-12 grid-rows-[50px_minmax(200px,_1fr)] p-4">
@@ -17,13 +17,13 @@ const BlogsPage = ({ posts, categories }) => {
 
 export default BlogsPage;
 
-export async function getServerSideProps(context) {
-  const { data } = await axios.get("http://localhost:3001/blogs");
-  const { data: categories } = await axios.get("http://localhost:3001/categories");
-  return {
-    props: {
-      posts: data,
-      categories,
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const { data } = await axios.get("http://localhost:3001/blogs");
+//   const { data: categories } = await axios.get("http://localhost:3001/categories");
+//   return {
+//     props: {
+//       posts: data,
+//       categories,
+//     },
+//   };
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/blogs");
-  });
 
-  return <div></div>;
+  return (
+    <div className="centering gap-8 min-h-screen">
+      <Link href="/blogs">
+        <a className="centering w-80 h-44 text-2xl font-bold border-2 transition-all duration-200 hover:border-blue-500 rounded-xl">بلاگ</a>
+      </Link>
+
+      <Link href="/panel_admin">
+        <a className="centering w-80 h-44 text-2xl font-bold border-2 transition-all duration-200 hover:border-blue-500 rounded-xl">پنل ادمین</a>
+      </Link>
+    </div>
+  );
 }
